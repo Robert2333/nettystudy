ca02eccf95efe8a9c19e607eb7753c935ea47a36
Robert23333 <372720441@qq.com>
2018-08-05 14:29:29 +0800

    echo服务端以及客户端

--- src/main/java/echo/EchoServer.java
+++ src/main/java/echo/EchoServer.java
@@ -1,4 +1,44 @@
 package echo;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.net.InetSocketAddress;
+
 public class EchoServer {
+    private static int port;
+
+    public EchoServer(int port) {
+        this.port = port;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        new EchoServer(8080).start();
+    }
+
+    private static void start() throws InterruptedException {
+        NioEventLoopGroup group = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(group).channel(NioServerSocketChannel.class).localAddress(new InetSocketAddress(port)).childHandler(new ChannelInitializer<SocketChannel>() { //7
+                @Override
+                public void initChannel(SocketChannel ch)
+                        throws Exception {
+                    ch.pipeline().addLast(
+                            new EchoServerHandler());
+                }
+            });
+            ChannelFuture f = serverBootstrap.bind().sync();            //8
+            System.out.println(EchoServer.class.getName() + " started and listen on " + f.channel().localAddress());
+            f.channel().closeFuture().sync();            //9
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+    }
 }
---
+++ src/main/java/echo/EchoServerHandler.java
@@ -0,0 +1,33 @@
+package echo;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.util.CharsetUtil;
+
+//适配器类
+@ChannelHandler.Sharable//表明所有的channel中可以分享
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    //每个信息入栈都会调用
+    public void channelRead(ChannelHandlerContext ctx,
+                            Object msg) {
+        ByteBuf in = (ByteBuf) msg;
+        System.out.println("Server received: " + in.toString(CharsetUtil.UTF_8));        //2
+        ctx.write(in);                            //3
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER)//4
+                .addListener(ChannelFutureListener.CLOSE);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx,
+                                Throwable cause) {
+        cause.printStackTrace();                //5
+        ctx.close();                            //6
+    }
+
+}
